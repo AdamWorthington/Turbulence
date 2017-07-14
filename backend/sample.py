48a3faf19578219aada3922af5e8a8e184d868ca
Adam <worthina@purdue.edu>
2017-07-14 15:16:47 -0400

    Did the annoying ass formatting for timeseries db

--- backend/sample.py
+++ backend/sample.py
@@ -1,50 +1,60 @@
-import json, requests
+import json, requests, time
 
 urls = [
 	[
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/heartbeat',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/voltage'
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/heartbeat",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/voltage"
 	],
-	['https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/heartbeat',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/voltage'
+	["https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/heartbeat",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/voltage"
 	],
-	['https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/heartbeat',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/temperature',
-	'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/voltage'
+	["https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/heartbeat",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/temperature",
+	"https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/voltage"
 	]
 ]
 
 def post(payload):
-	url = 'http://httpbin.org/post'
-	headers = {'content-type': 'application/json'}
+	url = "http://httpbin.org/post"
+	headers = {"content-type": "application/json"}
 	response = requests.post(url, data=json.dumps(payload), headers=headers)
 	print str(response.text)
 
 def main():
 	i = 0
+	temperature = 0
+	voltage = 0
 	while 1 :
 		j = 0
-		data = {}
-		data['id'] = i + 1
-		data['status'] = "ERR"
-		data['temperature'] = -1
-		data['voltage'] = -1
+		outer = {}
+		inner = {}
+		cur_time = str(int(time.time()))
+		outer["messageId"] = "turbulence-" + cur_time
+	
 		for val in urls[i]:
 			resp = requests.get(url=val)
 			o = json.loads(resp.text)
-			if (j == 0) and ('status' in o):
-				data['status'] = o['status']
-			elif (j == 1) and ('value' in o):
-				data['temperature'] = o['value']
-			elif (j == 2) and ('value' in o):
-				data['voltage'] = o['value']
+			if (j == 0) and ("status" in o):
+				status = o["status"]
+			elif (j == 1) and ("value" in o):
+				temperature = o["value"]
+			elif (j == 2) and ("value" in o):
+				voltage = o["value"]
+			inner["name"] = "turbulence-turbine-" + str(i + 1) + "-temperature"
+
+			datapoints = [cur_time, temperature, voltage]
+			dataset = [datapoints]
+			inner["datapoints"] = dataset
+			data_arr = [inner]
+			outer["body"] = data_arr
 			j = j + 1
 		#print str(data)
-		post(data)
+		post(outer)
 		i = (i + 1)% 3
 
 
 if __name__== "__main__":
   main()
+

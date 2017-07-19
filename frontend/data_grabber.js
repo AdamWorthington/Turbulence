e322623a9000bddd48302c5c2ac1f6d49271a279
LukeJFernandez <lukejohnfernandez@gmail.com>
2017-07-19 08:57:08 -0400

    cleaned up data grabber. still not grabbing from turbines

--- frontend/data_grabber.js
+++ frontend/data_grabber.js
@@ -4,24 +4,26 @@ $.ajax({
     success: function(data) {
         $('#turbine1').html(data);
         $('#turbine1-container').removeClass("red-jumbotron")
+        console.log("success: " + data)
     },
     error: function(error) {
+        console.log("error: " + error)
         error = "There is no data to display. Turbine 1 is down."
         $('#turbine1').html(error);
         $('#turbine1-container').addClass("red-jumbotron")
     }
 });
 $.ajax({
-    url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature',
+    url: 'http://echo.jsontest.com/key/value/one/two',
     dataType: 'jsonp',
     success: function(data) {
         $('#turbine2').html(data);
         $('#turbine2-container').removeClass("red-jumbotron")
-        console.log("success" + data)
+        console.log("success: " + data)
 
     },
     error: function(error) {
-        console.log("error" + error)
+        console.log("error: " + error)
         error = "There is no data to display. Turbine 2 is down."
         $('#turbine2').html(error);
         $('#turbine2-container').addClass("red-jumbotron")
@@ -34,12 +36,18 @@ $.ajax({
     success: function(data) {
         $('#turbine3').html(data);
         $('#turbine3-container').removeClass("red-jumbotron")
+        console.log("success: " + data)
 
     },
     error: function(error) {
+        console.log("error: " + error)
         error = "There is no data to display. Turbine 3 is down."
         $('#turbine3').html(error);
         $('#turbine3-container').addClass("red-jumbotron")
 
     }
+});
+
+$.getJSON("http://echo.jsontest.com/key/value/one/two", function(json) {
+    console.log("json data from jsontest.com: " + json);
 });

edcece959ccc5febc4e9177439f27c620d7fa1e1
LukeJFernandez <lukejohnfernandez@gmail.com>
2017-07-19 08:05:25 -0400

    refactor to log error instead of mock data var

--- frontend/data_grabber.js
+++ frontend/data_grabber.js
@@ -1,43 +1,45 @@
 $.ajax({
-  url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature',
-  dataType: 'jsonp',
-  success: function(data) {
-  $('#turbine1').html(data);
-  $('#turbine1-container').removeClass("red-jumbotron")
-  },
-  error: function(e) {
-        data = "There is no data to display. Turbine 1 is down."
+    url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature',
+    dataType: 'jsonp',
+    success: function(data) {
         $('#turbine1').html(data);
+        $('#turbine1-container').removeClass("red-jumbotron")
+    },
+    error: function(error) {
+        error = "There is no data to display. Turbine 1 is down."
+        $('#turbine1').html(error);
         $('#turbine1-container').addClass("red-jumbotron")
-  }
+    }
 });
 $.ajax({
-  url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature',
-  dataType: 'jsonp',
-  success: function(data) {
-  $('#turbine2').html(data);
-    $('#turbine2-container').removeClass("red-jumbotron")
-
-  },
-  error: function(e) {
-        data = "There is no data to display. Turbine 2 is down."
+    url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature',
+    dataType: 'jsonp',
+    success: function(data) {
         $('#turbine2').html(data);
+        $('#turbine2-container').removeClass("red-jumbotron")
+        console.log("success" + data)
+
+    },
+    error: function(error) {
+        console.log("error" + error)
+        error = "There is no data to display. Turbine 2 is down."
+        $('#turbine2').html(error);
         $('#turbine2-container').addClass("red-jumbotron")
 
-  }
+    }
 });
 $.ajax({
-  url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/temperature',
-  dataType: 'jsonp',
-  success: function(data) {
-  $('#turbine3').html(data);
-    $('#turbine3-container').removeClass("red-jumbotron")
-
-  },
-  error: function(e) {
-        data = "There is no data to display. Turbine 3 is down."
+    url: 'https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/temperature',
+    dataType: 'jsonp',
+    success: function(data) {
         $('#turbine3').html(data);
+        $('#turbine3-container').removeClass("red-jumbotron")
+
+    },
+    error: function(error) {
+        error = "There is no data to display. Turbine 3 is down."
+        $('#turbine3').html(error);
         $('#turbine3-container').addClass("red-jumbotron")
 
-  }
+    }
 });
